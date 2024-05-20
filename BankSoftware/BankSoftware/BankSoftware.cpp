4e478e6e8679ad2fb253666507a8ec4ad174282e
Michal Siudut <michal.siudut.ms@gmail.com>
2024-05-20 09:02:56 +0200

    feature-main-page-without-logut-button

--- BankSoftware/BankSoftware/BankSoftware.cpp
+++ BankSoftware/BankSoftware/BankSoftware.cpp
@@ -1,6 +1,10 @@
 #include "BankSoftware.h"
+#include "MainPageLogIn.h"
 #include "NewClientSignUp.h"
+
+#include <qmessagebox.h>
 #include <QDateTime>
+
 
 BankSoftware::BankSoftware(QWidget *parent)
     : QMainWindow(parent)
@@ -12,7 +16,9 @@ BankSoftware::BankSoftware(QWidget *parent)
 	timer->start(1000);
 
     connect(ui.exit_button, SIGNAL(clicked()), this, SLOT(exit_button_clicked()));
-	connect(ui.signup_button, SIGNAL(clicked()), this, SLOT(new_client_button_clicked()));
+	connect(ui.signup_button, SIGNAL(clicked()), this, SLOT(signup_button_clicked()));
+	connect(ui.log_button, SIGNAL(clicked()), this, SLOT(login_button_clicked())); 
+
 }
 
 BankSoftware::~BankSoftware()
@@ -35,4 +41,19 @@ void BankSoftware::signup_button_clicked()
 	NewClientSignUp* new_client = new NewClientSignUp();
 	new_client->show();
 	this->close();
+}
+void BankSoftware::login_button_clicked()
+{
+	QString admin_account_number = ui.line_edit_account_number->text();
+	QString admin_password = ui.line_edit_password->text();
+	if (admin_account_number == "11111111" && admin_password == "tajne") {
+		MainPageLogIn* new_client = new MainPageLogIn();
+		new_client->show();
+		this->close();
+	}
+	else
+	{
+		QMessageBox::information(this, "Login", "Takie konto nie istnieje, kliknij zarejestruj sie aby dolaczyc do naszego banku!");
+	}
+	
 }
